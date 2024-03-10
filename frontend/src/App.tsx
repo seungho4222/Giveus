ced93db387e16cf4d97cfbc111453e5e07305950
naerim <naerim1119@gmail.com>
2024-03-11 08:14:20 +0900

    ✨ feat : recoil 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,22 @@ import { ThemeProvider } from 'styled-components'
 import HomePage from '@pages/HomePage'
 import { darkTheme, lightTheme } from '@styles/theme'
 import { GlobalStyle } from '@styles/GlobalStyle'
+import useTheme from '@hooks/useTheme'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
+  const themeProps = useTheme()
+
   return (
     <>
       <div>
-        <ThemeProvider theme={'light' ? lightTheme : darkTheme}>
-          <GlobalStyle />
-          <HomePage />
+        <ThemeProvider
+          theme={themeProps.theme === 'light' ? lightTheme : darkTheme}
+        >
+          <BrowserRouter>
+            <GlobalStyle />
+            <HomePage />
+          </BrowserRouter>
         </ThemeProvider>
       </div>
     </>
