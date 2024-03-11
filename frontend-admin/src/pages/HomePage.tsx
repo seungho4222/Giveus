368020e9656b4d687e866ad40defe311843d6bfc
tmdwoghd <tmdwoghd@naver.com>
2024-03-11 17:56:22 +0900

    ✨ feat : svg 컴포넌트 생성

---
+++ frontend-admin/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ReactComponent as AdminLogo} from '@assets/icons/adminLogo.svg'
+
+const HomePage = () => {
+    return (
+        <div>
+            <AdminLogo/>
+            <h1>hello homepage!</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere architecto neque reprehenderit aspernatur similique qui, voluptatibus fugit amet dolorum, dolor rem facilis minus sunt reiciendis est ut consectetur, nesciunt pariatur!</p>
+        </div>
+    );
+};
+
+export default HomePage;
--- frontend-admin/src/router/router.tsx
+++ frontend-admin/src/router/router.tsx
@@ -1,11 +1,11 @@
-import Test from '@/pages/Test';
+import HomePage from '@pages/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test/>} />
+        <Route path="/" element={<HomePage/>} />
       </Routes>
     </BrowserRouter>
   );
