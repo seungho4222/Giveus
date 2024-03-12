3f596674d58e93166e2b5e8881d13cd57362ee09
yihoney <109622@naver.com>
2024-03-12 17:58:24 +0900

    ⚙️ config : backend funding project setting

--- backend-funding/src/main/java/com/giveus/funding/BackendFundingApplication.java
+++ backend-funding/src/main/java/com/giveus/funding/BackendFundingApplication.java
@@ -1,4 +1,4 @@
-package com.giveus.gateway;
+package com.giveus.funding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class BackendGatewayApplication {
+public class BackendFundingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendGatewayApplication.class, args);
+		SpringApplication.run(BackendFundingApplication.class, args);
 	}
 
 }
