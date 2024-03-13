8bae35122871bf9d1f81ecaee9364740e4179fa8
김내림 <naerim1119@naver.com>
2024-03-13 15:30:56 +0900

    🐛 fix : kakao url 수정

--- frontend/src/components/login/KakaoLogin/index.tsx
+++ frontend/src/components/login/KakaoLogin/index.tsx
@@ -1,12 +1,8 @@
 import * as k from '@components/login/KakaoLogin/KakaoLogin.styled'
 
 const Index = () => {
-  const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY
-  const REDIRECT_URI = import.meta.env.VITE_FRONT_BASE_URL + '/login/oauth'
-  // const REDIRECT_URI = 'http://localhost:8082/login/oauth2/token/kakao'
-
   const onClick = () => {
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+    const kakaoURL = `http://localhost:8082/oauth2/authorization/kakao`
     window.location.href = kakaoURL
   }
 
