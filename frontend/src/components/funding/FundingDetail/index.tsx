6db9ceeba997a138f16708640b08780143b3fb4e
seungho <kials1@naver.com>
2024-03-13 17:53:35 +0900

    ✨ feat : 펀딩 중복 라우팅

--- frontend/src/components/funding/FundingDetail/index.tsx
+++ frontend/src/components/funding/FundingDetail/index.tsx
@@ -2,34 +2,42 @@ import * as D from '@/components/funding/FundingDetail/FundingDetail.styled'
 import { FundingType } from '@/types/fundingType'
 import FundingDetailCommon from './FundingDetailCommon'
 import { useState } from 'react'
-import DetailMain from './DetailMain'
-import DonorList from './DonorList'
-import MedicalExpense from './MedicalExpense'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const Index = (props: { data: FundingType }) => {
   const { data } = props
-  const [tap, setTap] = useState('detailMain')
+  const [tap, setTap] = useState('detail-main')
+  const navigate = useNavigate()
 
-  const tapSection = () => {
-    switch (tap) {
-      case 'detailMain':
-        return <DetailMain data={data} />
-      case 'donorList':
-        return <DonorList />
-      case 'medicalExpense':
-        return <MedicalExpense />
-    }
+  const onClickHandler = (page: string) => {
+    setTap(page)
+    navigate(`/funding/${data.id}/${page}`)
   }
 
   return (
     <D.Container>
       <FundingDetailCommon data={data} />
       <D.Wrap>
-        <D.Tap $active={tap === 'detailMain'} onClick={() =>setTap("detailMain")}>소개</D.Tap>
-        <D.Tap $active={tap === 'donorList'} onClick={() =>setTap("donorList")}>기부자 명단</D.Tap>
-        <D.Tap $active={tap === 'medicalExpense'} onClick={() =>setTap("medicalExpense")}>진료비 사용 내역</D.Tap>
+        <D.Tap
+          $active={tap === 'detail-main'}
+          onClick={() => onClickHandler('detail-main')}
+        >
+          소개
+        </D.Tap>
+        <D.Tap
+          $active={tap === 'donor-list'}
+          onClick={() => onClickHandler('donor-list')}
+        >
+          기부자 명단
+        </D.Tap>
+        <D.Tap
+          $active={tap === 'medical-expense'}
+          onClick={() => onClickHandler('medical-expense')}
+        >
+          진료비 사용 내역
+        </D.Tap>
       </D.Wrap>
-      {tapSection()}
+      <Outlet />
     </D.Container>
   )
 }
--- frontend/src/routers/AuthRouter.tsx
+++ frontend/src/routers/AuthRouter.tsx
@@ -5,6 +5,9 @@ import AlarmPage from '@pages/alarm/AlarmPage'
 import FundingPage from '@pages/funding/FundingPage'
 import MyPage from '@pages/mypage/MyPage'
 import FundingDetailPage from '@/pages/funding/FundingDetailPage'
+import DetailMainPage from '@/pages/funding/DetailMainPage'
+import DonorListPage from '@/pages/funding/DonorListPage'
+import MedicalExpensePage from '@/pages/funding/MedicalExpensePage'
 
 const AuthRouter = () => {
   return (
@@ -14,7 +17,11 @@ const AuthRouter = () => {
       <Route path="/funding" element={<FundingPage />} />
       <Route path="/alarm" element={<AlarmPage />} />
       <Route path="/mypage" element={<MyPage />} />
-      <Route path="/funding/:id" element={<FundingDetailPage />} />
+      <Route path="/funding/:id" element={<FundingDetailPage />}>
+        <Route path="detail-main" element={<DetailMainPage />} />
+        <Route path="donor-list" element={<DonorListPage />} />
+        <Route path="medical-expense" element={<MedicalExpensePage />} />
+      </Route>
     </Routes>
   )
 }
