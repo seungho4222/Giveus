50f808fea6e6780c12066fe8c45cc4c1687533de
김내림 <naerim1119@naver.com>
2024-03-13 10:05:04 +0900

    ✨ feat : 카카오 로그인 url

--- frontend/src/components/login/KakaoLogin/index.tsx
+++ frontend/src/components/login/KakaoLogin/index.tsx
@@ -1,8 +1,17 @@
 import * as k from '@components/login/KakaoLogin/KakaoLogin.styled'
 
 const Index = () => {
+  const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY
+  const REDIRECT_URI = import.meta.env.VITE_FRONT_BASE_URL + '/login/oauth'
+  // const REDIRECT_URI = 'http://localhost:8082/login/oauth2/token/kakao'
+
+  const onClick = () => {
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+    window.location.href = kakaoURL
+  }
+
   return (
-    <k.Button>
+    <k.Button onClick={onClick}>
       <img src="icon/icon_kakao.png" alt="" />
       카카오로 시작하기
     </k.Button>
