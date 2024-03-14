d49c5444602e94eb1a15d45ab2942ada02231ef4
서지현 <seojihyeon99@naver.com>
2024-03-14 23:47:46 +0900

    ✨ feat : Gateway에 Jwt 토큰 검증 필터 추가

--- backend-gateway/src/main/java/com/giveus/gateway/common/util/JwtUtil.java
+++ backend-gateway/src/main/java/com/giveus/gateway/common/util/JwtUtil.java
@@ -1,5 +1,8 @@
 package com.giveus.gateway.common.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,57 +18,37 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JwtUtil {
 
-//    @Value("${jwt.secret}")
-//    private String secret;
-//
-//    @Value("${jwt.expiration.access}")
-//    private Long accessPeriod;
-//
-//    @Value("${jwt.expiration.refresh}")
-//    private Long refreshPeriod;
-//
-//    // 최초 로그인시 AccessToken과 RefreshToken을 발급
-//    public AuthTokenRes generateToken(String provider, String key) {
-//
-//        String accessToken = generateAccessToken(provider, key);
-//        String refreshToken = generateRefreshToken(provider, key);
-//
-//        return new AuthTokenRes(accessToken, refreshToken);
-//    }
-//
-//    public String generateRefreshToken(String provider, String key) {
-//        Claims claims = Jwts.claims().setSubject(key);
-//        claims.put("provider", provider);
-//
-//        Date now = new Date();
-//
-//        return Jwts.builder()
-//                .setClaims(claims)
-//                .setIssuedAt(now)
-//                .setExpiration(new Date(now.getTime() + refreshPeriod))
-//                .signWith(SignatureAlgorithm.HS256, secret)
-//                .compact();
-//    }
-//
-//    public String generateAccessToken(String provider, String key) {
-//        Claims claims = Jwts.claims().setSubject(key);
-//        claims.put("provider", provider);
-//
-//        Date now = new Date();
-//
-//        return Jwts.builder()
-//                .setClaims(claims)
-//                .setIssuedAt(now)
-//                .setExpiration(new Date(now.getTime() + accessPeriod))
-//                .signWith(SignatureAlgorithm.HS256, secret)
-//                .compact();
-//
-//    }
-//
-////    // 토큰에서 Email을 추출
-////    public static String getUid(String token) {
-////        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
-////    }
+    @Value("${jwt.secret}")
+    private static String secret;
 
+    // AccessToken이나 RefreshToken이 현재 유효한지 확인
+    public boolean verifyToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(secret) // 비밀키를 설정하여 파싱함
+                    .parseClaimsJws(token);  // 주어진 토큰을 파싱하여 Claims 객체를 얻음
+            // 토큰의 만료 시간과 현재 시간비교
+            return claims.getBody()
+                    .getExpiration()
+                    .after(new Date());  // 만료 시간이 현재 시간 이후인지 확인하여 유효성 검사 결과를 반환
+        } catch (Exception e) {
+            return false; // => 토큰이 유효하지 않다는 예외는 JWT 토큰을 검증하는 JwtAuthFilter에서 발생
+        }
+    }
+
+    // 토큰에서 소셜로그인 고유키 추출
+    public static String getSnsKey(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // 토큰에서 소셜로그인 종류 추출
+    public static String getProvider(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().get("provider", String.class);
+    }
+
+//    // 토큰에서 ROLE(권한)만 추출
+//    public String getRole(String token) {
+//        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().get("role", String.class);
+//    }
 
 }
