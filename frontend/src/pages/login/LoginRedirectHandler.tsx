be5798bd9fdcefff746159633c27b918fb6716df
김내림 <naerim1119@naver.com>
2024-03-15 09:15:21 +0900

    🌱 chore : 카카오 로그인 성공 후 메인으로 이동

--- frontend/src/pages/login/LoginRedirectHandler.tsx
+++ frontend/src/pages/login/LoginRedirectHandler.tsx
@@ -1,8 +1,11 @@
 import { loginSuccess } from '@apis/auth'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LoginRedirectHandler = () => {
   const url = new URL(window.location.href).searchParams
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     setStorage()
@@ -24,16 +27,16 @@ const LoginRedirectHandler = () => {
       // 로그인 성공
       localStorage.removeItem('accessToken')
       const accessToken = url.get('accessToken')
-      accessToken && loginSuccess({ accessToken })
+      accessToken &&
+        loginSuccess({ accessToken }).then(res => {
+          console.log('login success', res)
+          navigate('/')
+        })
     }
   }
 
-  // http://localhost:5173/login/oauth2?type=register&provider=kakao&email=jihyeon2474@gmail.com
-  // http://localhost:5173/login/oauth2?type=login&accessToken=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMzg2NTgxODU2IiwicHJvdmlkZXIiOiJrYWthbyIsImlhdCI6MTcxMDMxMzk4MSwiZXhwIjoxNzEwMzE3NTgxfQ.X_5k1v3dvPR7BXE7B9saULkd4Y2ErvXKchITICXNesc
-
   return (
     <div>
-      <button>임시 카카오 버튼</button>
       <div>login redirect page</div>
     </div>
   )
