5a58df6b7025252969c1cb7ad55c0620b3d9b67e
김내림 <naerim1119@naver.com>
2024-03-18 10:57:59 +0900

    🌱 chore :  검색 페이지 컴포넌트 생성

---
+++ frontend/src/components/search/SearchForm/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <input placeholder="검색어를 입력해주세요" />
+    </div>
+  )
+}
+
+export default Index
--- frontend/src/pages/search/SearchPage.tsx
+++ frontend/src/pages/search/SearchPage.tsx
@@ -1,10 +1,17 @@
 import Navbar from '@common/Navbar'
 import Layout from '@common/Layout'
+import SearchForm from '@components/search/SearchForm'
+import RecentTerm from '@components/search/RecentTerm'
+import SearchResult from '@components/search/SearchResult'
 
 const SearchPage = () => {
   return (
     <>
-      <Layout>search page</Layout>
+      <Layout>
+        <SearchForm />
+        <RecentTerm />
+        <SearchResult />
+      </Layout>
       <Navbar current="search" />
     </>
   )
