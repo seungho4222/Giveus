bc47f071779d21a2d57c9eed4e486e39c666500b
seungho <kials1@naver.com>
2024-03-19 00:46:28 +0900

    ✨ feat : fundingList 펀딩 총 수 div

--- frontend/src/components/funding/FilterBox/FilterBox.styled.tsx
+++ frontend/src/components/funding/FilterBox/FilterBox.styled.tsx
@@ -3,7 +3,8 @@ import { colors } from '@/styles/theme'
 
 export const Container = styled.div`
   display: flex;
-  margin: 45px auto 10px auto;
+  margin-inline: auto;
+  padding-block: 45px 10px;
   width: 90%;
   column-gap: 8px;
 `
---
+++ frontend/src/components/funding/FundingListCount/FundingListCount.styled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  width: 90%;
+  margin-inline: auto;
+`
+
+export const TotalCount = styled.div`
+  font-size: 0.8em;
+  font-weight: 700;
+`
