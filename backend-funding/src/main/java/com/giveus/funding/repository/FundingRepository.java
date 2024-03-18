d3a3601bfe51a0ecbf1bd31fb7e91de67d96274d
이하늬 <109622@naver.com>
2024-03-19 01:20:18 +0900

    ✨ feat : 펀딩 관리자 목록 조회 기능 추가

--- backend-funding/src/main/java/com/giveus/funding/repository/FundingRepository.java
+++ backend-funding/src/main/java/com/giveus/funding/repository/FundingRepository.java
@@ -3,6 +3,6 @@ package com.giveus.funding.repository;
 import com.giveus.funding.entity.Funding;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FundingRepository extends JpaRepository<Funding, Integer> {
+public interface FundingRepository extends JpaRepository<Funding, Integer>, FundingRepositoryCustom {
 
 }
