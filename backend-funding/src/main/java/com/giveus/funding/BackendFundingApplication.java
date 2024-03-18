729a39bfbacf867d547f51c9f9e1a3c66888db26
yihoney <109622@naver.com>
2024-03-18 16:14:08 +0900

    ⚙️ config : 펀딩 swagger 설정

--- backend-funding/src/main/java/com/giveus/funding/BackendFundingApplication.java
+++ backend-funding/src/main/java/com/giveus/funding/BackendFundingApplication.java
@@ -8,9 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class BackendFundingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendFundingApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFundingApplication.class, args);
+	}
 
 }
--- backend-funding/src/main/java/com/giveus/funding/repository/FundingRepository.java
+++ backend-funding/src/main/java/com/giveus/funding/repository/FundingRepository.java
@@ -3,8 +3,6 @@ package com.giveus.funding.repository;
 import com.giveus.funding.entity.Funding;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface FundingRepository extends JpaRepository<Funding, Integer> {
 
 }
