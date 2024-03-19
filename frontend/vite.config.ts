629b30bd1029e172250bd8430e5ea0e2f4410b33
박형규 <gudrb4869@naver.com>
2024-03-19 11:01:13 +0900

    ⚙️ config : vite-plugin-mkcert 설치, https 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPath from 'vite-tsconfig-paths'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPath()],
+  plugins: [react(), tsConfigPath(), mkcert()],
   server: {
     port: 3000,
     host: true,
