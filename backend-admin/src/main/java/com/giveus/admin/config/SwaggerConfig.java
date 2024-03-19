bff7044d1930086ee5b6d0e516b5f393d946ecca
yihoney <109622@naver.com>
2024-03-19 12:04:58 +0900

    ⚙️ config : admin swagger 설정

--- backend-admin/src/main/java/com/giveus/admin/config/SwaggerConfig.java
+++ backend-admin/src/main/java/com/giveus/admin/config/SwaggerConfig.java
@@ -9,12 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-//        servers = {
-////                @Server(url = "https://i10c111.p.ssafy.io", description = "배포 서버 API"),
-//                @Server(url = "http://localhost:8083", description = "로컬 API"),
-//        },
         info = @Info(
-                title = "GIVEUS 펀딩 API 명세서",
+                title = "GIVEUS 관리자 API 명세서",
                 description = "C206",
                 version = "1.0.0",
                 contact = @Contact(name = "giveus", email = "giveus2024@gmail.com"))
@@ -26,34 +22,8 @@ public class SwaggerConfig {
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("API")
-                .packagesToScan("com.giveus.funding.controller")
-//                .pathsToMatch("/api/**")
+                .packagesToScan("com.giveus.admin.controller")
                 .build();
     }
-//    @Value("${springdoc.swagger-ui.config-url}")
-//    private String server;
-//    @Bean
-//    public OpenAPI openAPI() {
-//        log.info(server);
-//        return new OpenAPI()
-////                .components(new Components())
-//                .info(apiInfo())
-////                .servers(List.of(server()))
-//                ;
-//    }
-//
-//    private Info apiInfo() {
-//        return new Info()
-//                .title("GIVEUS 펀딩 API 명세서")
-//                .description("C206")
-//                .version("1.0.0");
-//    }
-//
-//    private Server server() {
-//        return new Server()
-//                .url(server)
-//                .description("로컬 API");
-//
-//    }
 
 }
--- backend-funding/src/main/java/com/giveus/funding/config/SwaggerConfig.java
+++ backend-funding/src/main/java/com/giveus/funding/config/SwaggerConfig.java
@@ -9,10 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-//        servers = {
-////                @Server(url = "https://i10c111.p.ssafy.io", description = "배포 서버 API"),
-//                @Server(url = "http://localhost:8083", description = "로컬 API"),
-//        },
         info = @Info(
                 title = "GIVEUS 펀딩 API 명세서",
                 description = "C206",
@@ -27,33 +23,7 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("API")
                 .packagesToScan("com.giveus.funding.controller")
-//                .pathsToMatch("/api/**")
                 .build();
     }
-//    @Value("${springdoc.swagger-ui.config-url}")
-//    private String server;
-//    @Bean
-//    public OpenAPI openAPI() {
-//        log.info(server);
-//        return new OpenAPI()
-////                .components(new Components())
-//                .info(apiInfo())
-////                .servers(List.of(server()))
-//                ;
-//    }
-//
-//    private Info apiInfo() {
-//        return new Info()
-//                .title("GIVEUS 펀딩 API 명세서")
-//                .description("C206")
-//                .version("1.0.0");
-//    }
-//
-//    private Server server() {
-//        return new Server()
-//                .url(server)
-//                .description("로컬 API");
-//
-//    }
 
 }
