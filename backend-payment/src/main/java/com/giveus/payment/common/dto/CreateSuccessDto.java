dc39e809da40a4e9b938f274d4c20c36a0dd3f8f
박형규 <gudrb4869@naver.com>
2024-03-19 16:23:58 +0900

    ⚙️ config : payment 프로젝트 기본 세팅

---
+++ backend-payment/src/main/java/com/giveus/payment/common/dto/CreateSuccessDto.java
@@ -0,0 +1,16 @@
+package com.giveus.payment.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 등록 성공 시 응답할 DTO
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateSuccessDto {
+
+    private int id;
+}
