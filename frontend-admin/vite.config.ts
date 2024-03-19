2d9da23b9958b9f578bf380868a78db47d30883e
박형규 <gudrb4869@naver.com>
2024-03-19 11:25:02 +0900

    ⚙️ config : vite-plugin-mkcert 삭제 및 재설정

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
-import mkcert from 'vite-plugin-mkcert'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths(), svgr(), mkcert()],
+  plugins: [react(), tsConfigPaths(), svgr()],
   server: {
     port: 3001,
     host: true,
