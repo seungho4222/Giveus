0dd73cbae57b613c55fecbfaa7938d5bab3d5af8
yihoney <109622@naver.com>
2024-03-20 09:20:00 +0900

    ⚙️ config : swagger 서버 설정

--- backend-gateway/src/main/java/com/giveus/gateway/config/SwaggerConfig.java
+++ backend-gateway/src/main/java/com/giveus/gateway/config/SwaggerConfig.java
@@ -3,25 +3,22 @@ package com.giveus.gateway.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-            .info(new Info()
-                .title("GIVEUS API 명세서")
-                .description("C206")
-                .version("1.0.0")
-                .contact(new Contact()
-                    .email("giveus2024@gmail.com")
-                    .name("giveus")))
-            ;
+                .info(new Info()
+                        .title("GIVEUS API 명세서")
+                        .description("C206")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .email("giveus2024@gmail.com")
+                                .name("giveus")))
+                ;
     }
 
 }
