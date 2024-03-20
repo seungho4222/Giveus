d02e322a45c98b6becdb567ac0001fa08d1f8f81
김내림 <naerim1119@naver.com>
2024-03-20 10:39:30 +0900

    🎨 style : 알림페이지 스타일 적용

--- frontend/src/components/alarm/AlarmList/index.tsx
+++ frontend/src/components/alarm/AlarmList/index.tsx
@@ -1,8 +1,17 @@
-const Index = () => {
+import { AlarmType } from '@/types/alarmType'
+import AlarmListItem from '@components/alarm/AlarmList/AlarmListItem'
+import * as a from '@components/alarm/AlarmList/AlarmList.styled'
+
+const Index = (props: { data: AlarmType[] }) => {
+  const { data } = props
+
   return (
-    <div>
-      <div>알람 목록</div>
-    </div>
+    <a.Container>
+      {data &&
+        data.map(item => (
+          <AlarmListItem item={item} key={item.notificationNo} />
+        ))}
+    </a.Container>
   )
 }
 
