32007071b4298d82679999734f0e20b86319d322
서지현 <seojihyeon99@naver.com>
2024-03-20 15:17:00 +0900

    ⚙️ config : 알림 서버 초기 환경설정

---
+++ backend-notification/src/test/java/com/giveus/notification/BackendNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giveus.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
