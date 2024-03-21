f9f781c89e970d69fc723a141c0aa441e763d541
leeseungho <kials1@naver.com>
2024-03-21 13:12:46 +0900

    ♻️ refactor: 펀딩 필터 - selector 변경

--- frontend/src/pages/funding/FundingPage.tsx
+++ frontend/src/pages/funding/FundingPage.tsx
@@ -4,43 +4,24 @@ import HomeHeader from '@/components/home/HomeHeader'
 import { fundingState } from '@/stores/funding'
 import Layout from '@common/Layout'
 import Navbar from '@common/Navbar'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 import FundingListCount from '@/components/funding/FundingListCount'
 import { useEffect, useState } from 'react'
 import Modal from '@/common/Modal'
 import FilterCondition from '@/components/funding/FilterBox/FilterCondition'
 import ResponsiveModal from '@/common/ResponsiveModal'
 import SortCondition from '@/components/funding/FilterBox/SortCondition'
-import {
-  sortByDonerCount,
-  sortByHighestAmount,
-  sortByLatest,
-  sortByLowestAmount,
-  sortByPeriod,
-} from '@/utils/fundingSort'
-import { sortCondition } from '@/assets/data/fundingCondition'
 import FilterArea from '@/components/funding/FilterBox/FilterArea'
-import { filterByAge, filterByDoing, filterByDone } from '@/utils/fundingFilter'
-import {
-  ageRangeState,
-  filterStatusState,
-  filteredFundingState,
-  sortState,
-} from '@/stores/filterAndSort'
+import { filteredFundingState } from '@/stores/filterAndSort'
 import { useQuery } from '@tanstack/react-query'
 import { fetchFundingList } from '@/apis/funding'
 import FundingListBox from '@/components/funding/FundingListCard/FundingListBox'
 
 const FundingPage = () => {
-  const [funding, setFunding] = useRecoilState(fundingState)
+  const setFunding = useSetRecoilState(fundingState)
   const [filterOpen, setFilterOpen] = useState<boolean>(false) // 필터 모달
   const [sortOpen, setSortrOpen] = useState<boolean>(false) // 정렬 모달
-  const sort = useRecoilValue(sortState)
-  const filterStatus = useRecoilValue(filterStatusState)
-  const ageRange = useRecoilValue(ageRangeState)
-
-  const [filteredFunding, setFilteredFunding] =
-    useRecoilState(filteredFundingState)
+  const filteredFunding = useRecoilValue(filteredFundingState)
 
   const { data, isLoading } = useQuery({
     queryKey: ['FundingList'],
@@ -50,43 +31,6 @@ const FundingPage = () => {
   useEffect(() => {
     !isLoading && setFunding(data)
   }, [data, isLoading])
-
-  useEffect(() => {
-    const filter = () => {
-      let filteredData = funding
-
-      if (filterStatus[0] && !filterStatus[1]) {
-        filteredData = filterByDoing(filteredData)
-      } else if (!filterStatus[0] && filterStatus[1]) {
-        filteredData = filterByDone(filteredData)
-      }
-
-      if (filterStatus[2]) {
-        filteredData = filterByAge(filteredData, ageRange[0], ageRange[1])
-      }
-
-      return filteredData
-    }
-    const filteredData = filter()
-
-    switch (sort) {
-      case sortCondition[0]:
-        setFilteredFunding(sortByPeriod(filteredData))
-        break
-      case sortCondition[1]:
-        setFilteredFunding(sortByDonerCount(filteredData))
-        break
-      case sortCondition[2]:
-        setFilteredFunding(sortByLatest(filteredData))
-        break
-      case sortCondition[3]:
-        setFilteredFunding(sortByHighestAmount(filteredData))
-        break
-      case sortCondition[4]:
-        setFilteredFunding(sortByLowestAmount(filteredData))
-        break
-    }
-  }, [filterStatus, sort, funding])
 
   return (
     <>
