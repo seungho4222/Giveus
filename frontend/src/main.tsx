a73a4ab0aa8bea7ac31a1d85151d5a54bee99590
leeseungho <kials1@naver.com>
2024-03-21 10:12:32 +0900

    🌱 chore : react-query 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
 )
