2bf7e3fc7322e9f2eaf6c1b93b80fabd5700c7a2
김내림 <naerim1119@naver.com>
2024-03-22 17:42:47 +0900

    ✨ feat : 정기 결제 페이지 생성

---
+++ frontend/src/pages/mypage/regular/RegularPage.tsx
@@ -0,0 +1,11 @@
+import MypagePrevHeader from '@components/mypage/MypagePrevHeader'
+
+const RegularPage = () => {
+  return (
+    <div>
+      <MypagePrevHeader title="정기 결제" url="/mypage" />
+    </div>
+  )
+}
+
+export default RegularPage
