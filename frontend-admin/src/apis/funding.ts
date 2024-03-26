e9ec6352c3fd8b0befc9edc82018a0533d814749
seungho <kials1@naver.com>
2024-03-26 22:56:48 +0900

    ✨ feat : 관리자 펀딩목록 조회 api 확인 필요

--- frontend-admin/src/apis/funding.ts
+++ frontend-admin/src/apis/funding.ts
@@ -3,6 +3,6 @@ import { authRequest } from '@utils/requestMethods'
 const url = '/api/v1/admin/funding'
 
 // 펀딩 목록 조회
-export const fetchFundingList = async () => {
-  return authRequest.get(`${url}?adminNo=${1}`).then(res => res.data.data)
+export const fetchFundingList = async (adminNo: number) => {
+  return authRequest.get(`${url}?adminNo=${adminNo}`).then(res => res.data.data)
 }
