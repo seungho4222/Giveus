ffece030887577b52b085480276dcb565bdc02c9
박형규 <gudrb4869@naver.com>
2024-03-26 12:06:35 +0900

    ✨ feat : 카카오페이 결제 포인트 충전 API 구현

--- backend-payment/src/main/java/com/giveus/payment/dto/KakaoPayInfoDto.java
+++ backend-payment/src/main/java/com/giveus/payment/dto/KakaoPayInfoDto.java
@@ -1,6 +1,7 @@
 package com.giveus.payment.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -9,13 +10,20 @@ import lombok.ToString;
  * React에게서 받아오는 내용으로 가격과, 상품이름 받아오도록 설정했음.
  * 카카오 API에 Request로 보내고 싶은 정보를 더 추가해서 Custom해서 사용하면 될듯.
  */
+@Schema(description = "카카오페이 결제 준비 요청")
 @Getter
 @ToString
 public class KakaoPayInfoDto {
-    private int memberNo; // 회원 PK
-    private int fundingNo; // 펀딩 PK
-    private int price; // 후원 금액
-    private int point; // 사용 포인트
-    private String title; // 펀딩 제목
-    private boolean opened; // 공개 여부
+    @Schema(description = "회원 PK", example = "1")
+    private int memberNo;
+    @Schema(description = "펀딩 PK", example = "1")
+    private int fundingNo;
+    @Schema(description = "후원 금액 (카카오페이 결제 준비 요청 시 후원 금액은 최소 1원 이상이어야 함)", example = "1000")
+    private int price;
+    @Schema(description = "사용 포인트", example = "0")
+    private int point;
+    @Schema(description = "펀딩 제목", example = "펀딩 123")
+    private String title;
+    @Schema(description = "공개 여부", example = "true")
+    private boolean opened;
 }
