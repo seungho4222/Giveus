67cb5bc9deb941513b21d3bf05763057506d9e3b
tmdwoghd <tmdwoghd@naver.com>
2024-03-27 17:55:33 +0900

    🐛 fix : 네이버 OCR 수정

--- frontend-admin/src/pages/fundingregister/OCR.ts
+++ frontend-admin/src/pages/fundingregister/OCR.ts
@@ -7,83 +7,42 @@ const uuid = crypto.randomUUID()
 const invokeUrl = 'https://76mjhq6gjw.apigw.ntruss.com/custom/v1/29548/b76957781f2da1d145048bfe53cec9dff5380d89f1a338f6e71daad7f674490a/infer'
 const secretKey = 'U0tLVG1qcnNjZVNsYU9EaFBtUWtSaEx3c1pNVkR3b0I='
 
-// export function requestWithBase64(base64Data:string) {
-//   axios
-//     .post(
-//       invokeUrl, // APIGW Invoke URL
-//       {
-//         images: [
-//           {
-//             format: 'PNG', // file format
-//             name: 'diagnosis', // image name
-//             data: base64Data.split(',')[1], // image base64 string(only need part of data). Example: base64String.split(',')[1]
-//             templateIds:[29548]
-//           },
-//         ],
-//         requestId: uuid, // unique string
-//         timestamp: Date.now(),
-//         version: 'V2',
+
+export function requestWithBase64(base64Data:string) {
+  axios
+    .post(
+      invokeUrl, // APIGW Invoke URL
+      {
+        images: [
+          {
+            format: 'PNG', // file format
+            name: 'diagnosis', // image name
+            data: base64Data.split(',')[1], // image base64 string(only need part of data). Example: base64String.split(',')[1]
+            templateIds:[29548]
+          },
+        ],
+        requestId: uuid, // unique string
+        timestamp: Date.now(),
+        version: 'V2',
         
-//       },
-//       {
-//         headers: {
-//           'X-OCR-SECRET': secretKey, // Secret Key
-//           'Content-Type':'multipart/form-data',
-//         //   'Access-Control-Allow-Origin': 'http://localhost:3001',
-//         //   'Access-Control-Allow-Methods': 'post',
-//         },
-//       },
-//     )
-//     .then(res => {
-//       if (res.status === 200) {
-//         console.log('requestWithBase64 response:', res.data)
-//       }
-//     })
-//     .catch(e => {
-//       console.warn('requestWithBase64 error', e.response)
-//     })
-// }
+      },
+      {
+        headers: {
+          'X-OCR-SECRET': secretKey, // Secret Key
+        },
+      },
+    )
+    .then(res => {
+      if (res.status === 200) {
+        console.log('requestWithBase64 response:', res.data)
+      }
+    })
+    .catch(e => {
+      console.warn('requestWithBase64 error', e.response)
+    })
+}
 
-// export function requestWithFile(imgURL:string) {
-//   const file = '' // image file object. Example: fs.createReadStream('./example.png')
-//   const message = {
-//     images: [
-//       {
-//         format: 'png', // file format
-//         name: 'diagnosis', // file name
-//         templateIds:[29548]
-//       },
-//     ],
-//     requestId: uuid, // unique string
-//     timestamp: Date.now(),
-//     version: 'V2',
-//   }
 
-//   const formData = new FormData()
-
-//   formData.append('file', file)
-//   formData.append('message', JSON.stringify(message))
-
-//   axios
-//     .post(
-//       invokeUrl, // APIGW Invoke URL
-//       formData,
-//       {
-//         headers: {
-//           'X-OCR-SECRET': secretKey, // Secret Key
-//           ...formData.getHeaders(),
-//         },
-//       },
-//     )
-//     .then(res => {
-//       if (res.status === 200) {
-//         console.log('requestWithFile response:', res.data)
-//       }
-//     })
-//     .catch(e => {
-//       console.warn('requestWithFile error', e.response)
-//     })
-// }
 
 
 export function requestWithFile(imageUrl: string) {
@@ -116,8 +75,7 @@ export function requestWithFile(imageUrl: string) {
           {
             headers: {
               'X-OCR-SECRET': secretKey, // Secret Key
-              'Content-Type': 'multipart/form-data', // Content-Type 설정
-            //   ...formData.getHeaders(),
+              // ...formData.getHeaders(),
             },
           },
         )
@@ -134,3 +92,4 @@ export function requestWithFile(imageUrl: string) {
       console.error('Error fetching image:', error);
     });
 }
+
