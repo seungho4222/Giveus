3ba7a43dd1acbc430b428698c76ec880399c3dd5
naerim <naerim1119@gmail.com>
2024-03-28 20:17:14 +0900

    🐛 fix : vite.config.ts 수정

--- frontend/src/components/login/KakaoLogin/index.tsx
+++ frontend/src/components/login/KakaoLogin/index.tsx
@@ -3,8 +3,10 @@ import * as k from '@components/login/KakaoLogin/KakaoLogin.styled'
 const Index = () => {
   const { DEV, VITE_SERVER_URL } = import.meta.env
   const onClick = () => {
-    const mode = DEV ? VITE_SERVER_URL : ''
-    const kakaoURL = `${mode}/oauth2/authorization/kakao`
+    const kakaoURL = DEV
+      ? `${VITE_SERVER_URL}/oauth2/authorization/kakao`
+      : '/oauth2/authorization/kakao'
+
     //const kakaoURL = `${BASE_URL}/oauth2/authorization/kakao`
     window.location.href = kakaoURL
   }
