0df3193e1420a25289a43dc58613a963e07fc61e
naerim <naerim1119@gmail.com>
2024-03-28 20:00:59 +0900

    🐛 fix : Index.html 수정

--- frontend/src/components/login/KakaoLogin/index.tsx
+++ frontend/src/components/login/KakaoLogin/index.tsx
@@ -1,18 +1,16 @@
-// import { BASE_URL } from '@utils/requestMethods'
 import * as k from '@components/login/KakaoLogin/KakaoLogin.styled'
 
 const Index = () => {
-  const { VITE_SERVER_URL } = import.meta.env
-
+  const { DEV, VITE_SERVER_URL } = import.meta.env
   const onClick = () => {
-    const kakaoURL = `/oauth2/authorization/kakao`
+    const mode = DEV ? VITE_SERVER_URL : ''
+    const kakaoURL = `${mode}/oauth2/authorization/kakao`
     //const kakaoURL = `${BASE_URL}/oauth2/authorization/kakao`
     window.location.href = kakaoURL
   }
 
   return (
     <k.Button onClick={onClick}>
-      {VITE_SERVER_URL}
       <img src="icon/icon_kakao.png" alt="" />
       카카오로 시작하기
     </k.Button>
