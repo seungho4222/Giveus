352b5df818ba07766e40e01f9ab2af217458d461
leeseungho <kials1@naver.com>
2024-03-30 23:56:37 +0900

    ✨ feat : funding-sort-participants  추가

--- frontend/src/utils/fundingSort.tsx
+++ frontend/src/utils/fundingSort.tsx
@@ -1,21 +1,21 @@
 import { FundingType } from "@/types/fundingType"
 
 export const sortByPeriod = (data: FundingType[]): FundingType[] => {
-  return data.slice().sort((a, b) => new Date(a.endDate).getTime() - new Date(b.endDate).getTime());
+  return data.slice().sort((a, b) => new Date(a.endDate).getTime() - new Date(b.endDate).getTime())
 }
 
 export const sortByDonerCount = (data: FundingType[]): FundingType[] => {
-  return data // 모금자 수 연결하기
+  return data.slice().sort((a, b) => b.donationCnt - a.donationCnt)
 }
 
 export const sortByLatest = (data: FundingType[]): FundingType[] => {
-  return data.slice().sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+  return data.slice().sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
 }
 
 export const sortByHighestAmount = (data: FundingType[]): FundingType[] => {
-  return data.slice().sort((a, b) => b.totalAmount - a.totalAmount);
+  return data.slice().sort((a, b) => b.totalAmount - a.totalAmount)
 }
 
 export const sortByLowestAmount = (data: FundingType[]): FundingType[] => {
-  return data.slice().sort((a, b) => a.totalAmount - b.totalAmount);
+  return data.slice().sort((a, b) => a.totalAmount - b.totalAmount)
 }
