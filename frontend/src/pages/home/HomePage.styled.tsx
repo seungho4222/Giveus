3d688bd83c1dda7c445814ecde4a26bf82e72182
김내림 <naerim1119@naver.com>
2024-03-31 02:35:12 +0900

    🌱 chore : 메인페이지 스크롤 수정

---
+++ frontend/src/pages/home/HomePage.styled.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Wrap = styled.div`
+  height: 100vh;
+  padding-bottom: 60px;
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+    width: 0;
+    height: 0;
+    background: transparent;
+    -webkit-appearance: none;
+  }
+`
