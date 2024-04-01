08f827d816b7685a3f75e4583fd7618bffb8c012
tmdwoghd <tmdwoghd@naver.com>
2024-04-01 13:07:11 +0900

    🐛 fix : 기금 사용 내역 OCR

--- frontend-admin/src/components/fundingDetail/UsageReg/index.tsx
+++ frontend-admin/src/components/fundingDetail/UsageReg/index.tsx
@@ -10,8 +10,8 @@ import { BooleanStateType } from '@/types/commonType'
 import DetailFile from '@components/fundingDetail/DetailFile'
 import { OCRResult, RegDataType } from '@/types/fundingType'
 
-const index = (props: BooleanStateType) => {
-  const { setValue } = props
+const Index = (props: BooleanStateType) => {
+  const { value, setValue } = props
   const selectedFundingNo = useRecoilValue(selectedFundingNoState)
   const [regData, setRegData] = useState<UsageDataType>({
     fundingNo: selectedFundingNo,
@@ -39,50 +39,50 @@ const index = (props: BooleanStateType) => {
     if (results) {
       console.log('결과 받았어', results)
     }
-
-    const handleCreateFundingUsage = async () => {
-      mutate(regData)
-    }
-
-    return (
-      <u.Container>
-        {' '}
-        <DetailFile onOCRResult={handleOCRResult} />
-        <UsageInput
-          id="category"
-          label="검사 종목"
-          placeholder="검사 종목을 입력해주세요"
-          value={regData.category}
-          setValue={setRegData}
-        />
-        <UsageInput
-          id="content"
-          label="검사 내용"
-          placeholder="검사 내용을 입력해주세요"
-          value={regData.content}
-          setValue={setRegData}
-        />
-        <UsageInput
-          id="count"
-          label="검사 횟수"
-          placeholder="검사 횟수을 입력해주세요"
-          value={regData.count}
-          setValue={setRegData}
-        />
-        <UsageInput
-          id="amount"
-          label="검사 비용"
-          placeholder="검사 비용을 입력해주세요"
-          value={regData.amount}
-          setValue={setRegData}
-        />
-        <u.Wrap>
-          <u.Button onClick={() => handleCreateFundingUsage()}>
-            기금 사용 내역 등록
-          </u.Button>
-        </u.Wrap>
-      </u.Container>
-    )
   }
+
+  const handleCreateFundingUsage = async () => {
+    mutate(regData)
+  }
+
+  return (
+    <u.Container>
+      <DetailFile onOCRResult={handleOCRResult} />
+      <UsageInput
+        id="category"
+        label="검사 종목"
+        placeholder="검사 종목을 입력해주세요"
+        value={regData.category}
+        setValue={setRegData}
+      />
+      <UsageInput
+        id="content"
+        label="검사 내용"
+        placeholder="검사 내용을 입력해주세요"
+        value={regData.content}
+        setValue={setRegData}
+      />
+      <UsageInput
+        id="count"
+        label="검사 횟수"
+        placeholder="검사 횟수을 입력해주세요"
+        value={regData.count}
+        setValue={setRegData}
+      />
+      <UsageInput
+        id="amount"
+        label="검사 비용"
+        placeholder="검사 비용을 입력해주세요"
+        value={regData.amount}
+        setValue={setRegData}
+      />
+      <u.Wrap>
+        <u.Button onClick={() => handleCreateFundingUsage()}>
+          기금 사용 내역 등록
+        </u.Button>
+      </u.Wrap>
+    </u.Container>
+  )
 }
-export default index
+
+export default Index
