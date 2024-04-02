c46f657aa31cafbfc0044cc88ebdad16327254b4
yihoney <109622@naver.com>
2024-04-02 14:41:40 +0900

    ✨ feat : 후원 시 캐싱 업데이트 하도록 기존 펀딩 삭제하게 설정

--- backend-payment/src/main/java/com/giveus/payment/BackendPaymentApplication.java
+++ backend-payment/src/main/java/com/giveus/payment/BackendPaymentApplication.java
@@ -2,12 +2,14 @@ package com.giveus.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableDiscoveryClient
+@EnableCaching
 public class BackendPaymentApplication {
 
     public static void main(String[] args) {
