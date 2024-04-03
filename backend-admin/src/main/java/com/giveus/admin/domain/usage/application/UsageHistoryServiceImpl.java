bf6c4f14ae6f308c4569ee84263ca0e87b16a612
yihoney <109622@naver.com>
2024-04-03 20:04:02 +0900

    ♻️ refactor : 사용 내역 등록 시 리스트로 요청 받도록 수정

--- backend-admin/src/main/java/com/giveus/admin/domain/usage/application/UsageHistoryServiceImpl.java
+++ backend-admin/src/main/java/com/giveus/admin/domain/usage/application/UsageHistoryServiceImpl.java
@@ -4,9 +4,9 @@ import com.giveus.admin.global.common.response.CreateSuccessDto;
 import com.giveus.admin.domain.usage.dto.FundingUsageCreateReq;
 import com.giveus.admin.domain.funding.domain.Funding;
 import com.giveus.admin.domain.usage.domain.UsageHistory;
-import com.giveus.admin.global.error.exception.InvalidRequestException;
 import com.giveus.admin.domain.usage.dao.UsageHistoryRepository;
 import com.giveus.admin.domain.funding.application.FundingService;
+import com.giveus.admin.global.error.exception.InvalidRequestException;
 import com.giveus.admin.infra.sms.CoolSmsClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,28 +32,38 @@ public class UsageHistoryServiceImpl implements UsageHistoryService {
 
     @Override
     @Transactional
-    public CreateSuccessDto createFundingUsage(FundingUsageCreateReq req) {
-
-        Funding funding = fundingService.getFundingEntity(req.getFundingNo());
+    public CreateSuccessDto createFundingUsage(List<FundingUsageCreateReq> reqList) {
 
         // 진행중인 펀딩이라면 사용 내역 등록 요청 시 예외 발생 처리
 //        if (!fundingService.isDoneFunding(funding)) {
 //            throw new InvalidRequestException();
 //        }
-        
-        // 최초 등록일 시에 문자 전송
-        if (getUsageHistoryList(req.getFundingNo()).isEmpty()) {
+
+        if (reqList.isEmpty()) { // 빈 배열이라면 예외 발생 시키기
+            throw new InvalidRequestException();
+        }
+
+        Funding funding = fundingService.getFundingEntity(reqList.get(0).getFundingNo());
+        for (FundingUsageCreateReq req : reqList) {
+
+            // 펀딩 사용 내역 등록
+            UsageHistory usageHistory = UsageHistoryTransfer.dtoToEntity(funding, req);
+            usageHistoryRepository.save(usageHistory);
+        }
+
+        // 최초 등록일 시에 알림, 문자 전송
+        if (getUsageHistoryList(funding).size() == reqList.size()) {
 
             // 펀딩에 참여한 기부자들에게 알림 전송 (microservice간 통신)
             String requestUrl = usageHistoryUrl + funding.getFundingNo();
-
-            ResponseEntity<String> response = restTemplate.exchange(
+            restTemplate.exchange(
                     requestUrl, // 요청 URL
                     HttpMethod.POST, // 요청 메서드
                     null, // 요청 본문
                     String.class // 응답 타입
             );
 
+            // 펀딩 수혜자에게 후기 등록할 링크 문자 전송
             String msg = "[giveus]\n" +
                     "안녕하세요, 기브어스입니다.\n" +
                     "아래의 링크에서 후원 후기를 나눠주세요!\n" +
@@ -61,15 +71,11 @@ public class UsageHistoryServiceImpl implements UsageHistoryService {
             coolSmsClient.send(funding.getPhone(), msg, funding.getRegId());
         }
 
-        // 펀딩 사용 내역 등록
-        UsageHistory usageHistory = UsageHistoryTransfer.dtoToEntity(funding, req);
-        UsageHistory savedUsageHistory = usageHistoryRepository.save(usageHistory);
-
-        return new CreateSuccessDto(savedUsageHistory.getUsageHistoryNo());
+        return new CreateSuccessDto(funding.getFundingNo());
     }
 
-    private List<UsageHistory> getUsageHistoryList(int fundingNo) {
-        return usageHistoryRepository.findByFunding_FundingNo(fundingNo);
+    private List<UsageHistory> getUsageHistoryList(Funding funding) {
+        return usageHistoryRepository.findByFunding(funding);
     }
 
 }
