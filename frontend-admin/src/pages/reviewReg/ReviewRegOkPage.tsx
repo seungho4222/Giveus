cb5580ac7c1150ac71967a4b8528f464e28a133b
leeseungho <kials1@naver.com>
2024-04-03 21:32:24 +0900

    🐛 fix : reviewOkPage text  변경

--- frontend-admin/src/pages/reviewReg/ReviewRegOkPage.tsx
+++ frontend-admin/src/pages/reviewReg/ReviewRegOkPage.tsx
@@ -20,14 +20,14 @@ const ReviewRegOkPage = () => {
   return (
     <r.Container>
       <r.Top>
-        <r.Title>펀딩 등록 완료</r.Title>
+        <r.Title>후기 등록 완료</r.Title>
         <Lottie options={defaultOptions} width={328} height={239} />
-        <r.SubTitle>펀딩이 정상적으로 등록되었습니다.</r.SubTitle>
+        <r.SubTitle>후기가 정상적으로 등록되었습니다.</r.SubTitle>
       </r.Top>
       <r.Bottom>
         <r.Line />
         <r.Desc>
-          <span>* 등록된 펀딩은 GIVEUS 어플에서 확인가능합니다.</span>
+          <span>* 등록된 후기는 GIVEUS 어플에서 확인가능합니다.</span>
           <span>* 문의사항은 GIVEUS에 문의해주세요.</span>
         </r.Desc>
         <FullButton text="확인" disabled={false} onClick={goMainPage} />
